eb780c292055237535833de213ab5f80cbe147ba
Paul-Émile AILLAUD <aillaud.pe@gmail.com>
2022-05-08 20:32:27 +0200

    un peu de js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,32 @@
-function verif(tel) {
-    if ((tel.length == 10)&& (tel[0] == 0)&& (tel[1] == 7 || tel[1] == 6)) {
-        console.log("true")
+var checkCGU = document.getElementById('cgu');
+
+
+function toggleCGU() {
+    if (checkCGU.checked) {
+        document.getElementById('submit').disabled = false;
     } else {
-        console.log("false")
+        document.getElementById('submit').disabled = true;
+    }
+}
+
+function checkNumber(tel) {
+    if (tel.length !== 10) {
+        return false;
+
+    } else if (tel[0] !== "0") {
+        return false;
+    } else if (tel[1] !== "6" && tel[1] !== "7") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validerForm() {
+    let form = document.forms['contact'];
+    let num = form.elements['tel'].value;
+    if (!checkNumber(num)) {
+        alert("Veuillez entrer un numéro de téléphone valide");
+        return;
     }
 }
