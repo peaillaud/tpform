62c22046fbb126284c83ff5f68c2ab74f3571b50
Paul-Émile AILLAUD <aillaud.pe@gmail.com>
2022-05-09 22:23:00 +0200

    comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+/**
+ * Fonction qui vérifie si la case pour accepter les CGU est cochée, et si oui, active le bouton de soumission du formulaire
+ */
+
 function toggleCGU() {
     var checkCGU = document.getElementById('cgu');
     if (checkCGU.checked) {
@@ -6,6 +10,12 @@ function toggleCGU() {
         document.getElementById('submit').disabled = true;
     }
 }
+
+/**
+ * Fonction qui vérifie si le numéro de téléphone fourni est valide, retourne true si oui, false si non.
+ * @param { String } tel 
+ * @returns { Boolean }
+ */
 
 function checkNumber(tel) {
     if (tel.length !== 10) {
@@ -20,6 +30,11 @@ function checkNumber(tel) {
     }
 }
 
+/**
+ * Fonction qui vérifie si l'entièreté du formulaire est valide, retourne true si oui, false si non.
+ * @returns { Boolean }
+ */
+
 function validerForm() {
     let form = document.forms['contact'];
     let num = form.elements['tel'].value;
@@ -28,6 +43,10 @@ function validerForm() {
     }
     return true;
 }
+
+/**
+ * Fonction qui, pour chaque élément du formulaire, affiche sa valeur dans un paragraphe situé en dessous.
+ */
 
 
 function displayOnHtml() {
@@ -41,6 +60,9 @@ function displayOnHtml() {
     });
 }
 
+/**
+ * Fonction qui exécute la vérification du formulaire et affiche les résultats s'il est valide.
+ */
 
 function validerTotal() {
     if (validerForm()) {
